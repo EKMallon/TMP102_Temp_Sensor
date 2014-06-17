c4c07a9c9bd2ae49aa3d239ba517598b3e7d5556
EKMallon <edward.mallon@gmail.com>
2014-06-16 19:37:21 -0500

    format tweaks

--- _20140611_TMP102_BasicSketch_12Bit_OneShotMode/_20140611_TMP102_BasicSketch_12Bit_OneShotMode.ino
+++ _20140611_TMP102_BasicSketch_12Bit_OneShotMode/_20140611_TMP102_BasicSketch_12Bit_OneShotMode.ino
@@ -3,10 +3,6 @@
 // Up to four devices on one bus, ACCURACY: 0.5°C (–25°C to +85°C), One lsb is 0.0625°C
 // Datasheet: http://www.ti.com/lit/ds/sbos397b/sbos397b.pdf
 
-//char foo;  //this is bogus code to fix a software bug in the compiler see: http://forum.arduino.cc/index.php?topic=84412.0
-
-#include <Wire.h> 
-
 // comments from  https://github.com/systronix/Arduino-TMP102/blob/master/TMP102_proof_13Jul20.ino
 /** --------  Addressing --------
 if ADDR is GND, address is 0x48 (default for the sparkfun board)
@@ -26,25 +22,26 @@ There are four pointer addresses:
 01  Configuration Register (Read/Write) 16 bits, MSB first
 10  TLOW Limit Register (Read/Write)
 11  THIGH Limit Register (Read/Write)
-
 */
+
+#include <Wire.h> 
 
 // 3 color indicator LED pin connections
 #define RED_PIN 3
 #define BLUE_PIN 5
 #define GREEN_PIN 4
 
-byte errorflag=0;
-
 #define TMP102_CFG_default_byte1 B01100001  // 12 bit WITH ShutDown bit turned ON as well
 #define TMP102_CFG_default_byte2 B10100000  // just the defaults from pg 7
 #define TMP102_OneShotBit B10000000  // one-shot by ORing D7 of CFG byte 1 to 1
 
-int TMP102_I2C_ADDR = 0x48;  // 72=base address (four possible values)
-
 // 1-byte pointer to write to tmp102 BEFORE reading back 2 bytes of data from that register
 #define TMP102_TEMP_REG_pointer 0x00  // temperature register, read only, 16bits
 #define TMP102_CONF_REG_pointer 0x01  // config register, read/write, 16 bits
+
+int TMP102_I2C_ADDR = 0x48;  // 72=base address (four possible values)
+byte errorflag=0;
+
 
 void setup() 
 { 
@@ -61,7 +58,7 @@ void setup()
   Wire.write(TMP102_CFG_default_byte1);  //Sets to 12bit, sd mode on
   Wire.write(TMP102_CFG_default_byte2);  //none of these settings matter in one shot mode, at my temperature range, but set them to defaults anyway
   errorflag = Wire.endTransmission();
-  if ( errorflag != 0) {Serial.print ("TMP102 initial reg writinr failed, Error =");Serial.println((int)errorflag);errorflag=0;}
+  if ( errorflag != 0) {Serial.print ("Initial control reg writing on TMP102 failed!");errorflag=0;}
   
   // set one-shot bit to "1" - starts a single conversion then sleeps the sensor
   Wire.beginTransmission(TMP102_I2C_ADDR);
@@ -69,10 +66,10 @@ void setup()
   Wire.write(TMP102_CFG_default_byte1 | TMP102_OneShotBit); // Start one-shot conversion 40μA during conv
   //Wire.write(TMP102_CFG_default_byte2);  //dont need to bother writing the second byte
   errorflag = Wire.endTransmission();
-  if ( errorflag != 0) {Serial.print ("OOPS! problem setting OneSHOT bit =");Serial.println((int)errorflag);errorflag=0;} 
+  if ( errorflag != 0) {Serial.print ("OOPS! Problem setting OneSHOT bit! ");errorflag=0;} 
   
   getTMP102();  
-  Serial.println("...TMP102 has been initialized");
+  Serial.println("Success:TMP102 has been initialized");
  
 } 
 
@@ -119,7 +116,7 @@ void getTMP102() {
   const int Temp16 = (TempByte1 << 4) | (TempByte2 >> 4);    // builds 12-bit value
   Serial.print("Integer data before conversion: ");Serial.println(Temp16);
   deg_c = Temp16*0.0625;
-  Serial.print("Temperature in deg C is = ");Serial.println(deg_c,4);
+  Serial.print("Temperature in deg C = ");Serial.println(deg_c,4);
   
 }
 
